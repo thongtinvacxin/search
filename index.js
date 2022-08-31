6c6f20193af89a041879b559bfa101014535fe02
buidathieu <35317761+buidathieu@users.noreply.github.com>
2022-08-31 10:28:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+var table;
+function initTableData() {
+	
+    //Data from an json ?
+    $.getJSON("./Data.txt", function(responseData) {
+    	table = $('#example').DataTable({
+    	"processing": true,
+    	data: responseData,
+    	columns:[
+    		{ data: 'STT' },		
+    		{ data: 'TÊN VẮC XIN' },
+    		{ data: 'NSX' },
+    		{ data: 'PHÒNG BỆNH' },
+    		{ data: 'GIÁ' },
+		{ data: 'SỐ MŨI TIÊM' }, 
+		{ data: 'THÔNG TIN VẮC XIN' },
+		{ data: 'ĐỐI TƯỢNG' },
+		{ data: 'Phác đồ, lịch tiêm' },                            
+		{ data: 'Điều kiện trước tiêm' },
+		{ data: 'Khoảng cách với các vắc xin khác' },   
+		{ data: 'Phản ứng sau tiêm chủng' },
+		{ data: 'Các câu hỏi thường gặp' }, 
+		{ data: 'HÌNH ẢNH' } 
+    	]
+    	});
+    }).fail(function() {
+    	alert( "Cannot get data from URL" );
+    });
+}
 
 $(document).ready(function (){
 	initTableData();
@@ -9,5 +38,10 @@ $(document).ready(function (){
         	$(this).css("background-color", "lightblue");
     	}, 
 	});
-	
+	$("#btnReloadData").on("click", function(){
+		//alert("reload data...")
+		table.ajax.reload();
+	});
 });
+
+
